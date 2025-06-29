8d953e8a8affb1bbbe04a3ec616cfc62d22ff896
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-29 17:32:23 +0800

    netlifytoml

--- tanstackrouter202507050418-git.bat
+++ tanstackrouter202507050418-git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\tanstackrouter202507050418"
 git init
 git add .
-git commit -m "tanstackrouter202507050418"
+git commit -m "netlifytoml"
 git remote add origin https://github.com/abuhg17/tanstackrouter202507050418.git
 git checkout -b main
 git push -u origin main --force
